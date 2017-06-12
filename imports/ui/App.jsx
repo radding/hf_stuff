8fcd2504d8e402a63e9a6afa3e6cb4193d088957
Yoseph Radding <yoseph@shuttl.io>
2017-06-12 18:39:00 -0400

    some more ui updates

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,28 +6,40 @@ export default class App extends Component {
         super(props);
         
         this.state = {
-            phone_number: ""
+            phone_number: "",
+            section_num: 0
         }
         this.handleChange = this.handleChange.bind(this);
+        this.continue_on = this.continue_on.bind(this);
+        this.getSection = this.getSection.bind(this);
     }
   
   handleChange(event) {
     
   }
 
+  continue_on() {
+
+  }
+
+  getSection() {
+
+  }
+
   render() {
     return (
       <div className="container">
         <header>
-          <h1>Call Your Grand Mother Sam!</h1>
+          <h1>Call Your Grand Mother!</h1>
         </header>
 
         <div className="main">
-            <input 
-              name="phone_number" 
-              placeholder="phone number" 
-              value={this.state.username} 
-              onChange={this.handleChange} />
+            <div className="section">
+              {this.getSection()}
+            </div>
+            <div className="continue">
+              <button onClick={this.continue_on}> Continue </button>
+            </div>
         </div>
       </div>
     );
