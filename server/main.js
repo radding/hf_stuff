44e0786daef3da59b6646a70c96c26d339980f02
Jonathan Lehto <lehtoj21@gmail.com>
2017-06-12 20:05:58 -0400

    Started with server's js

--- server/main.js
+++ server/main.js
@@ -1,5 +1,26 @@
 import { Meteor } from 'meteor/meteor';
 
+var TWILIO_ACCOUNT_SID = "AC8bbf6f4380d3aafecfba11101dafb562";
+var TWILIO_AUTH_TOKEN = "099fe76a62edd7501687eb932a71b215";
+var twilio = require('twilio');
+var client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+var clientNumber = "";
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+function getValues() {
+	//get some html id's content
+	//get some html id's content
+}
+
+//set up onclick
+function sendPassword() {
+	getValues();
+	client.sendMessage({
+	  to: clientNumber,
+	  from: 'Grandma',
+	  body: 'Ahoy from Twilio!'
+	});
+}
